face543be72e3052b3d6279652f2a133d70a1237
Ivanildo Dias <ivanildodiasdesigner@gmail.com>
2015-04-15 11:35:13 -0300

    Ajustando o author.php e author-info.php

--- author.php
+++ author.php
@@ -1,26 +1,54 @@
-<?php get_header(); ?>
-	
-<!-- principal -->
-<div id="principal" class="col_8">
-	
-	<h1 id="page-title"><?php _e( 'Author Archives: ', 'viking-theme' ); echo get_the_author(); ?></h1>
-	
-	<main role="main">
-		<?php if ( get_the_author_meta( 'description' ) ) : ?>
-			<!-- post-info -->
-			<div id="post-info">
-				<?php get_template_part( 'author-info' ); ?>
-			</div>
-			<!-- /post-info -->
-		<?php endif; ?>
-		
-		<?php get_template_part( 'loop' ); ?>
-		
-		<?php get_template_part( 'pagination' ); ?>
-		
-	</main>
-	
-</div>
-<!-- /principal -->
+<?php
+/**
+ * O template para exibir as páginas de arquivos de Autores
+ * 
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ * 
+ * @package Estúdio Viking
+ * @since 1.0
+ */
 
-<?php get_footer(); ?>
+get_header();
+?>
+	
+<div id="principal" class="col_8">
+	<main id="main-content" role="main">
+	
+		<section id="page-header">
+			<h1 id="page-title"><?php _e( 'Author Archives: ', 'viking-theme' ); echo get_the_author(); ?></h1>
+			<?php if ( get_the_author_meta( 'description' ) ) : ?>
+				<div id="author-bio">
+					<?php get_template_part( 'author-info' ); ?>
+				</div><!-- #author-bio -->
+			<?php endif; ?>
+		</section><!-- #page-header -->
+		
+		<?php
+			if ( have_posts() ):
+				// Início do Loop
+				while ( have_posts() ) : the_post();
+					
+					/**
+					 * Inclui o template do formato de postagem específico para o conteúdo.
+					 * Se você quiser usar isso em um child theme, inclua um arquivo chamado content-____.php
+					 * (onde ____ é o formato de postagem) e que será utilizado em seu lugar.
+					 */
+					get_template_part( 'content', get_post_format() );
+				
+				endwhile;
+				
+				// Paginação de artigos
+				viking_post_pagination();
+				
+			else:
+				// Se não houver conteúdo, inclui o template "Nenhum artigo encontrado".
+				get_template_part( 'content', 'none' );
+				
+			endif;
+		?>
+		
+	</main><!-- #main-content -->
+</div><!-- #principal -->
+
+<?php
+get_footer();
